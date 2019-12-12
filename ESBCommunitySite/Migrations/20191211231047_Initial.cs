dab7176bc9454509dd728091802922b0eebb5a5f
leseastingray <lesea@my.lanecc.edu>
2019-12-11 17:40:51 -0800

    db server stuff all works

---
+++ ESBCommunitySite/Migrations/20191211231047_Initial.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ESBCommunitySite.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Instrument = table.Column<string>(nullable: true),
+                    DateJoined = table.Column<string>(nullable: true),
+                    Board = table.Column<bool>(nullable: false),
+                    SectionLeader = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageInfoID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Recipient = table.Column<string>(nullable: false),
+                    MessageText = table.Column<string>(nullable: false),
+                    Sender = table.Column<string>(nullable: false),
+                    MessageDate = table.Column<DateTime>(nullable: false),
+                    MessagePriority = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageInfoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PotentialMembers",
+                columns: table => new
+                {
+                    PotentialMemberID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Instrument = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    RequestDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PotentialMembers", x => x.PotentialMemberID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "PotentialMembers");
+        }
+    }
+}
---
+++ ESBCommunitySite/obj/Release/netcoreapp2.2/win-x86/PubTmp/EFSQLScripts/ESBCommunitySite.Repositories.AppDbContext.sql
@@ -0,0 +1,63 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20191211231047_Initial')
+BEGIN
+    CREATE TABLE [Members] (
+        [MemberID] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        [Instrument] nvarchar(max) NULL,
+        [DateJoined] nvarchar(max) NULL,
+        [Board] bit NOT NULL,
+        [SectionLeader] bit NOT NULL,
+        CONSTRAINT [PK_Members] PRIMARY KEY ([MemberID])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20191211231047_Initial')
+BEGIN
+    CREATE TABLE [Messages] (
+        [MessageInfoID] int NOT NULL IDENTITY,
+        [Recipient] nvarchar(max) NOT NULL,
+        [MessageText] nvarchar(max) NOT NULL,
+        [Sender] nvarchar(max) NOT NULL,
+        [MessageDate] datetime2 NOT NULL,
+        [MessagePriority] nvarchar(max) NULL,
+        CONSTRAINT [PK_Messages] PRIMARY KEY ([MessageInfoID])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20191211231047_Initial')
+BEGIN
+    CREATE TABLE [PotentialMembers] (
+        [PotentialMemberID] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Instrument] nvarchar(max) NOT NULL,
+        [Email] nvarchar(max) NOT NULL,
+        [RequestDate] datetime2 NOT NULL,
+        CONSTRAINT [PK_PotentialMembers] PRIMARY KEY ([PotentialMemberID])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20191211231047_Initial')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20191211231047_Initial', N'2.2.6-servicing-10079');
+END;
+
+GO
+
